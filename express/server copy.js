3958bb2cd90040372e0417adef9efb933d8aa511
Pavel Pribyl <pribyl.pavel@gmail.com>
2020-01-06 01:32:37 +0100

    Try

---
+++ express/server copy.js
@@ -0,0 +1,58 @@
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const serverless = require('serverless-http');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const aboutRouter = require('./routes/about');
+
+const app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+const router = express.Router();
+
+// app.use('/', indexRouter);
+// app.use('/users', usersRouter);
+// app.use('/about', aboutRouter);
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { page: 'Home', menuId: 'home' });
+});
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+app.use(bodyParser.json());
+app.use('/.netlify/functions/server', router);  // path must route to lambda
+app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
+
+module.exports = app;
+module.exports.handler = serverless(app);
