9364408806c448e2521475f6502c685bda1da826
Pavel Pribyl <pavelp@salsitasoft.com>
2020-02-14 13:24:39 +0100

    Compression

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const compression = require('compression');
 
 const indexRouter = require('./routes/index');
 const galleryRouter = require('./routes/gallery');
@@ -17,16 +18,21 @@ app.locals.price = '10 605 000';
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(compression());
+
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/galerie', galleryRouter);
 app.use('/kontakt', contactRouter);
 app.use('/detaily', detailsRouter);
+
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 
 // catch 404 and forward to error handler
